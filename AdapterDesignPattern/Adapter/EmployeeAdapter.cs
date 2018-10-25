640559d4ce2cb3cee4dcb508981a2b6196d038ec
Bojan Bogdanovski <freeboy_bb@yahoo.co.uk>
2018-10-25 20:33:42 +0200

    Add project files.

---
+++ AdapterDesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace AdapterDesignPattern.Adapter
+{
+    public class EmployeeAdapter : EmployeeManager, IEmployee
+    {
+        public override string GetAllEmployees()
+        {
+            string returnXml = base.GetAllEmployees();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(returnXml);
+            return JsonConvert.SerializeObject(doc, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
